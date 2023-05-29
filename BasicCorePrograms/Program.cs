df3f61ca19e9b5939363cfa191643fa4c45a40f0
Sumanone8 <sumanjhabhl@gmail.com>
2023-05-29 22:20:26 +0530

    Implemented Swap program

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -4,16 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter the dividend: ");
-        int dividend = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter the divisor: ");
-        int divisor = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
 
-        int quotient = dividend / divisor;
-        int remainder = dividend % divisor;
+        Console.WriteLine($"Before swapping: num1 = {num1}, num2 = {num2}");
 
-        Console.WriteLine($"Quotient: {quotient}");
-        Console.WriteLine($"Remainder: {remainder}");
+        // Swapping logic using a temporary variable
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        Console.WriteLine($"After swapping: num1 = {num1}, num2 = {num2}");
     }
 }
