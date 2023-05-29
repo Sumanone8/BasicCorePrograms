c81994b5a2e3bf88f090aa5ce850cd8aa6b87677
Sumanone8 <sumanjhabhl@gmail.com>
2023-05-29 22:11:39 +0530

    Quotient and Remainder Program Implemented

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -4,43 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        if (args.Length == 1)
-        {
-            int number = Convert.ToInt32(args[0]);
+        Console.Write("Enter the dividend: ");
+        int dividend = Convert.ToInt32(Console.ReadLine());
 
-            if (number > 0)
-            {
-                Console.Write($"Prime factors of {number}: ");
-                PrintPrimeFactors(number);
-            }
-            else
-            {
-                Console.WriteLine("Invalid input. Number should be a positive integer.");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Invalid input. Please provide a single command-line argument.");
-        }
-    }
+        Console.Write("Enter the divisor: ");
+        int divisor = Convert.ToInt32(Console.ReadLine());
 
-    static void PrintPrimeFactors(int number)
-    {
-        for (int i = 2; i * i <= number; i++)
-        {
-            while (number % i == 0)
-            {
-                Console.Write($"{i} ");
-                number /= i;
-            }
-        }
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
 
-        if (number > 1)
-        {
-            Console.Write($"{number} ");
-        }
-
-        Console.WriteLine();
+        Console.WriteLine($"Quotient: {quotient}");
+        Console.WriteLine($"Remainder: {remainder}");
     }
 }
-
